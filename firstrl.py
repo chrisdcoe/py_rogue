b9a79380995c1d610edef6a5d9fb64467cacf76b
Christopher Coe <christopherdcoe@gmail.com>
2018-07-15 17:20:32 -0500

    Added drawing to offscreen console, object generalization

--- firstrl.py
+++ firstrl.py
@@ -18,30 +18,65 @@ def handle_keys():
 
     # movement keys
     if libtcod.console_is_key_pressed(libtcod.KEY_UP):
-        playery -= 1
+        player.move(0, -1)
     elif libtcod.console_is_key_pressed(libtcod.KEY_DOWN):
-        playery += 1
+        player.move(0, 1)
     elif libtcod.console_is_key_pressed(libtcod.KEY_LEFT):
-        playerx -= 1
+        player.move(-1, 0)
     elif libtcod.console_is_key_pressed(libtcod.KEY_RIGHT):
-        playerx += 1
+        player.move(1, 0)
+
+# Class definitions
+class Object:
+    # a generic object: player, monster, item, stairs, etc
+    # always represented by a character on Screen
+    def __init__(self, x, y, char, color):
+        self.x = x
+        self.y = y
+        self.char = char
+        self.color = color
+
+    def move(self, dx, dy):
+        # move by the given amount
+        self.x += dx
+        self.y += dy
+
+    def draw(self):
+        # set the color and then draw the character that represents this object at its position
+        libtcod.console_set_default_foreground(con, self.color)
+        libtcod.console_put_char(con, self.x, self.y, self.char, libtcod.BKGND_NONE)
+
+    def clear(self):
+        # erase the character that represents the Object
+        libtcod.console_put_char(con, self.x, self.y, ' ', libtcod.BKGND_NONE)
+
 
 # Initialization
 libtcod.console_set_custom_font('arial10x10.png', libtcod.FONT_TYPE_GRAYSCALE | libtcod.FONT_LAYOUT_TCOD)
 libtcod.console_init_root(SCREEN_WIDTH, SCREEN_HEIGHT, 'python/libtcod tutorial', False) # Screen size, title, fullscreen
+con = libtcod.console_new(SCREEN_WIDTH, SCREEN_HEIGHT)
 libtcod.sys_set_fps(LIMIT_FPS) # Limits FPS if the game is in real time
 
-# Initialize player position to center of Screen
-playerx = SCREEN_WIDTH/2
-playery = SCREEN_HEIGHT/2
+# Create objects
+player = Object(SCREEN_WIDTH/2, SCREEN_HEIGHT/2, '@', libtcod.white)
+npc = Object(SCREEN_WIDTH/2 - 5, SCREEN_HEIGHT/2, '@', libtcod.yellow)
+objects = [npc, player] # list that holds all objects in the game
+
 # Main Loop
 while not libtcod.console_is_window_closed():
-    libtcod.console_set_default_foreground(0, libtcod.white)
-    libtcod.console_put_char(0, playerx, playery, '@', libtcod.BKGND_NONE)
+    # draw objects in list
+    for object in objects:
+        object.draw()
 
+    # blit the contents of the new offscreen console to the root console
+    libtcod.console_blit(con, 0, 0, SCREEN_WIDTH, SCREEN_HEIGHT, 0, 0, 0)
+    # present changes to the screen
     libtcod.console_flush()
 
-    libtcod.console_put_char(0, playerx, playery, ' ', libtcod.BKGND_NONE)
+    # erase objects from previous locations
+    for object in objects:
+        object.clear()
+
     # handle keys and exit game if needed
     exit = handle_keys()
     if exit:
