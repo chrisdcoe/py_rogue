4258a6c188bf5c8db7c96af412d4ad53ce8f391e
Christopher Coe <christopherdcoe@gmail.com>
2018-07-16 11:24:42 -0500

    Define the map and draw it to screen

--- firstrl.py
+++ firstrl.py
@@ -1,10 +1,59 @@
 import libtcodpy as libtcod
 
+# window size
 SCREEN_WIDTH = 80
 SCREEN_HEIGHT = 50
+
+# size of the map
+MAP_WIDTH = 80
+MAP_HEIGHT = 45
+
 LIMIT_FPS = 20
 
-# Keypress Functions
+color_dark_wall = libtcod.Color(0, 0, 100) # dark blue
+color_dark_ground = libtcod.Color(50, 50, 150) # dark indigo
+
+#########################
+### CLASS DEFINITIONS ###
+#########################
+
+class Object:
+    # a generic object: player, monster, item, stairs, etc
+    # always represented by a character on Screen
+    def __init__(self, x, y, char, color):
+        self.x = x
+        self.y = y
+        self.char = char
+        self.color = color
+
+    def move(self, dx, dy):
+        if not map[self.x + dx][self.y + dy].blocked:
+            # move by the given amount
+            self.x += dx
+            self.y += dy
+
+    def draw(self):
+        # set the color and then draw the character that represents this object at its position
+        libtcod.console_set_default_foreground(con, self.color)
+        libtcod.console_put_char(con, self.x, self.y, self.char, libtcod.BKGND_NONE)
+
+    def clear(self):
+        # erase the character that represents the Object
+        libtcod.console_put_char(con, self.x, self.y, ' ', libtcod.BKGND_NONE)
+
+class Tile:
+    # a map tile and its properties
+    def __init__(self, blocked, block_sight = None):
+        self.blocked = blocked
+
+        # by default, if a tile is blocked, it will also block sight
+        if block_sight is None: block_sight = blocked
+        self.block_sight = block_sight
+
+#################
+### FUNCTIONS ###
+#################
+
 def handle_keys():
 
     # misc keys
@@ -25,30 +74,40 @@ def handle_keys():
     elif libtcod.console_is_key_pressed(libtcod.KEY_RIGHT):
         player.move(1, 0)
 
-# Class definitions
-class Object:
-    # a generic object: player, monster, item, stairs, etc
-    # always represented by a character on Screen
-    def __init__(self, x, y, char, color):
-        self.x = x
-        self.y = y
-        self.char = char
-        self.color = color
+def make_map():
+    global map
 
-    def move(self, dx, dy):
-        # move by the given amount
-        self.x += dx
-        self.y += dy
+    # fill map with unblocked tiles
+    map = [[ Tile(False)
+        for y in range(MAP_HEIGHT) ]
+            for x in range(MAP_WIDTH) ]
 
-    def draw(self):
-        # set the color and then draw the character that represents this object at its position
-        libtcod.console_set_default_foreground(con, self.color)
-        libtcod.console_put_char(con, self.x, self.y, self.char, libtcod.BKGND_NONE)
+    # some pillars to test the blocked status
+    map[30][22].blocked = True
+    map[30][22].block_sight = True
+    map[50][22].blocked = True
+    map[50][22].block_sight = True
 
-    def clear(self):
-        # erase the character that represents the Object
-        libtcod.console_put_char(con, self.x, self.y, ' ', libtcod.BKGND_NONE)
+def render_all():
+    # draw all objects in list
+    for object in objects:
+        object.draw()
 
+    # go through all tiles and draw them to screen with appropriate background color
+    for y in range(MAP_HEIGHT):
+        for x in range(MAP_WIDTH):
+            wall = map[x][y].block_sight
+            if wall:
+                libtcod.console_set_char_background(con, x, y, color_dark_wall, libtcod.BKGND_SET)
+            else:
+                libtcod.console_set_char_background(con, x, y, color_dark_ground, libtcod.BKGND_SET)
+    
+    # blit the contents of the new offscreen console to the root console
+    libtcod.console_blit(con, 0, 0, SCREEN_WIDTH, SCREEN_HEIGHT, 0, 0, 0)
+
+##################
+### GAME LOGIC ###
+##################
 
 # Initialization
 libtcod.console_set_custom_font('arial10x10.png', libtcod.FONT_TYPE_GRAYSCALE | libtcod.FONT_LAYOUT_TCOD)
@@ -61,14 +120,14 @@ player = Object(SCREEN_WIDTH/2, SCREEN_HEIGHT/2, '@', libtcod.white)
 npc = Object(SCREEN_WIDTH/2 - 5, SCREEN_HEIGHT/2, '@', libtcod.yellow)
 objects = [npc, player] # list that holds all objects in the game
 
+# Construct the map
+make_map()
+
 # Main Loop
 while not libtcod.console_is_window_closed():
-    # draw objects in list
-    for object in objects:
-        object.draw()
-
-    # blit the contents of the new offscreen console to the root console
-    libtcod.console_blit(con, 0, 0, SCREEN_WIDTH, SCREEN_HEIGHT, 0, 0, 0)
+    # render all objects
+    render_all()
+    
     # present changes to the screen
     libtcod.console_flush()
 
